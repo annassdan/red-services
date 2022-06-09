015df778d488ad5567f6a1222d4e047473593fd4
Annas Dan <annassdan@gmail.com>
2022-06-09 16:21:25 +0700

    update helper wpp etc for report

--- routes/reports/hubungan-panjang-berat.js
+++ routes/reports/hubungan-panjang-berat.js
@@ -9,6 +9,7 @@ const {
     resolveRscriptCommand,
     rscript
 } = require("../../helpers/utilities");
+const {pool} = require("../../database/database");
 const router = express.Router();
 
 /**
@@ -51,6 +52,65 @@ router.post('/', async (req, res) => {
     res.status(200).json({
         status: responseStatus(1),
         graphicImageName: `${graphicImageName}${__image_extention}`
+    });
+});
+
+router.post('/wpp', (req, res, next) => {
+    const {start, end} = req.body;
+    pool.query(`with source as (select trim(wpp) as wpp
+                from brpl_biologireproduksi
+                where tanggal_sampling between '${start}' and '${end}')
+                select wpp as value, wpp as label
+                from source
+                group by wpp
+                order by wpp`, (error, {rows}) => {
+
+        if (error) {
+            res.status(500).json('Gagal');
+            return;
+        }
+
+        res.status(200).json(rows || []);
+    });
+});
+
+router.post('/locations', (req, res, next) => {
+    const {start, end, wpp} = req.body;
+    pool.query(`with source as (select trim(nama_lokasi_sampling) as nama_lokasi_sampling
+                from brpl_biologireproduksi
+                where tanggal_sampling between '${start}' and '${end}' and trim(wpp) = trim('${wpp}'))
+                select nama_lokasi_sampling as value, nama_lokasi_sampling as label
+                from source
+                group by nama_lokasi_sampling
+                order by nama_lokasi_sampling`, (error, {rows}) => {
+
+        if (error) {
+            res.status(500).json('Gagal');
+            return;
+        }
+
+        res.status(200).json(rows || []);
+    });
+});
+
+
+router.post('/species', (req, res, next) => {
+    const {start, end, wpp, location} = req.body;
+    pool.query(`with source as (select trim(uuid_spesies) as spesies
+                    from brpl_biologireproduksi
+                    where tanggal_sampling between '${start}' and '${end}'
+                    and trim(wpp) = trim('${wpp}') and trim(nama_lokasi_sampling) = trim('${location}'))
+                    select spesies as value, spesies as label
+                    from source
+                    group by spesies
+                    order by spesies`, (error, {rows}) => {
+
+        if (error) {
+            res.status(500).json('Gagal');
+            return;
+        }
+
+        res.status(200).json(rows || []);
     });
 });
 
